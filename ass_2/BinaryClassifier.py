fad338f828b7748496820c2aed242126dd35311c
Manish Kumar Bachhu <43577942+ManishKumarBachhu@users.noreply.github.com>
2020-04-24 12:04:49 +0530

    Added fscore and normal distribution, bias terms are equal to zero

--- ass_2/BinaryClassifier.py
+++ ass_2/BinaryClassifier.py
@@ -6,12 +6,14 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 class BinaryClassifier:
-	def __init__(self, input_dim, hidden_layers):
+	def __init__(self, input_dim, hidden_layers, weights_initialization = "uniform"):
 		#Activation Functions and their derivatives
 		self.sigmoid = lambda x: 1/(1+np.exp(-x))
 		self.relu = lambda x: np.where(x>0, x, 0)
 		self.sigmoid_der = lambda x: x*(1-x)
 		self.relu_der = lambda x: np.where(x>0,1,0)
+		self.tanh = lambda x: (2/(1 + np.exp(-2*x))) - 1 
+		self.tanh_der = lambda x: 1 - x**2
 
 		self.input_dim = input_dim
 		self.output_dim = 1
@@ -24,26 +26,32 @@ class BinaryClassifier:
 
 		self.network = []
 		self.outputs = []
-		self.bias = []
+		self.bias = [np.zeros(l) for l in layers[1:]]
 		self.delta = []
 		#Building network : Each layer = [W^[l-no], W^[l_no + 1]]
 		for l_no in range(len(layers) - 1):
+			if weights_initialization == "uniform":
 			#Initialising weights as random
-			self.network.append(np.random.rand(layers[l_no], layers[l_no+1]))
-			#self.network.append(np.full((layers[l_no], layers[l_no+1]),1.0))
+				self.network.append(np.random.rand(layers[l_no], layers[l_no+1]))
+			elif weights_initialization == "normal" :
+				self.network.append(np.random.normal(size = (layers[l_no], layers[l_no+1])))
+			elif weights_initialization == "ones" :
+				self.network.append(np.full((layers[l_no], layers[l_no+1]),1.0))
 
 	#Trains the network and returns a list of accuracies and loss over the training epochs
-	def train(self, X, Y, lr=0.5, epochs=10):
+	def train(self, X, Y, lr=0.5, epochs=10, use_bias = False):
 		acc_list = []
 		loss_list = []
+		fscore_list = []
 		for epoch in range(epochs):
 			self.forward_pass(X)
-			self.backward_pass(Y)
+			self.backward_pass(Y, use_bias)
 			self.update_weights(lr)
-			acc, loss = self.evaluate(X,Y)
+			acc, loss , fscore= self.evaluate(X,Y)
 			acc_list.append(acc)
 			loss_list.append(loss)
-		return [acc_list,loss_list]
+			fscore_list.append(fscore)
+		return [acc_list,loss_list,fscore_list]
 
 	#Does forward pass and stores the output for all of the training examples in outputs
 	def forward_pass(self, X):
@@ -51,16 +59,17 @@ class BinaryClassifier:
 		#First Layer outputs are the inputs itself
 		self.outputs.append(X)
 		for l_no, layer in enumerate(self.network):
-			inputs = self.outputs[-1]
+			layer = np.append(layer, self.bias[l_no].reshape(1,self.bias[l_no].shape[0]), axis = 0)
+			inputs = np.append(self.outputs[-1], np.full((self.outputs[-1].shape[0],1) , 1), axis = 1)
 			if l_no != len(self.network) - 1:
 				#ReLU is activation for hidden layers
-				self.outputs.append(self.relu(np.matmul(inputs,layer)))
+				self.outputs.append(self.tanh(np.matmul(inputs,layer)))
 			else:
 				#Sigmoid is activation for last layer
 				self.outputs.append(self.sigmoid(np.matmul(inputs,layer)))
 
 	#L2 loss is assumed
-	def backward_pass(self, Y):
+	def backward_pass(self, Y, use_bias):
 		deltas = []
 		last = True
 		dels = []
@@ -79,9 +88,11 @@ class BinaryClassifier:
 				out = []
 				for i in range(layer.shape[0]):
 					out.append(np.dot(layer[i],deltas[-1][j]))
-				dels.append(np.multiply(out,self.relu_der(self.outputs[-(l_no+2)][j])))
+				dels.append(np.multiply(out,self.tanh_der(self.outputs[-(l_no+2)][j])))
 			deltas.append(np.array(dels).reshape(Y.shape[0],len(layer)))
 		self.deltas = [d for d in reversed(deltas)]
+		if use_bias == True:
+			self.bias = [(1/len(Y))*np.sum(np.array(d), axis = 0) for d in self.deltas]
 
 	#Updates weights using the deltas calculated in the backward pass
 	def update_weights(self, lr):
@@ -94,7 +105,10 @@ class BinaryClassifier:
 					self.deltas[l_no][j].reshape(1,len(self.deltas[l_no][j])))
 
 	def evaluate(self, X, Y):
-		correct = 0
+		tp = 0 #True positives
+		fp = 0 #False positives
+		tn = 0 #True negatives
+		fn = 0 #False negatives
 		self.forward_pass(X)
 		for n in range(X.shape[0]):
 			if self.outputs[-1][n] >= 0.5:
@@ -103,16 +117,35 @@ class BinaryClassifier:
 				y_pred = 0
 			#print(Y[n], y_pred)
 			if Y[n] == y_pred:
-				correct += 1
+				if Y[n] == 1:
+					tp += 1
+				else:
+					tn += 1
+			else:
+				if y_pred == 1:
+					fp += 1
+				else:
+					fn += 1
 		loss = 0
 		#loss = half*sum(out - y)^2
 		for n in range(X.shape[0]):
 			loss += (self.outputs[-1][n] - Y[n])**2
 
-		return ((correct/X.shape[0])*100, loss/2)
+		precision = tp/(tp+fp)
+		recall = tp/(tp + fn)
+		fscore = (2*precision*recall)/(precision+recall) #Harmonic mean of precision and recall
+
+		return (((tp + tn)/X.shape[0])*100, loss/len(X), fscore)
 
 
-nn = BinaryClassifier(10,[20, 20])
+input_dim = 10
+hidden_layers = [20,20]
+weights_initialization = "normal"
+nn = BinaryClassifier(input_dim,hidden_layers, weights_initialization)
+print("Input dimension = ", input_dim)
+print("Hidden Layers = ", hidden_layers)
+print("Weights Initialising = ", weights_initialization)
+
 df = pd.read_csv('housepricedata.csv')
 label = 'AboveMedianPrice'
 X = df.drop(label, axis = 1).values
@@ -121,22 +154,24 @@ scaler = MinMaxScaler()
 X = scaler.fit_transform(X)
 
 Y = df[label].values
-'''
-encoder = LabelEncoder()
-encoder.fit(Y)
-Y = encoder.transform(Y)
-'''
+
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2)
 lr = 0.1
 epochs = 1000
 print("Training: (lr = ",lr ," epochs = ", epochs, ")")
 metrics = nn.train(X_train, Y_train, lr, epochs)
+#print(nn.bias)
+
 plt.plot(np.arange(len(metrics[0])), metrics[0])
-plt.savefig("Accuracy.eps")
+plt.savefig("Training Accuracy.eps")
 plt.close()
 
 plt.plot(np.arange(len(metrics[1])), metrics[1])
-plt.savefig("Loss.eps")
+plt.savefig("Training Loss.eps")
+plt.close()
+
+plt.plot(np.arange(len(metrics[2])), metrics[2])
+plt.savefig("Training Fscore.eps")
 plt.close()
 
 print("Evaluation:")
